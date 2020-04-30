00b46d364aae4687c2f5d6a3c471fa8304da2e7a
Amy Zhou <amy_zhou@college.harvard.edu>
2020-04-30 18:17:25 +1000

    added the block vis back

--- app.py
+++ app.py
@@ -134,5 +134,47 @@ def user_id(username):
 
     return render_template("username.html", projects=projects, studios=studios, username=username, user=authentication.get_login_info())
 
+@app.route("/challenges", methods=["GET", "POST"])
+def get_challenge():
+    if request.method == "GET":
+        return render_template("submit_challenge.html")
+    else:
+        scraper = Scraper()
+        parser = Parser()
+        project_url = request.form['project-url']
+        project_id = scraper.get_id(project_url)
+        downloaded_project = scraper.download_project(project_id)
+        results = parser.blockify(scratch_data=downloaded_project)
+
+        block_of_interest = "operator_random"
+
+        child = parser.get_child_blocks(results["blocks"][block_of_interest][0], downloaded_project)
+        sprite = parser.get_sprite(results["blocks"][block_of_interest][0], downloaded_project)
+        surround = parser.get_surrounding_blocks(results["blocks"][block_of_interest][0], downloaded_project, 7)
+
+        block_list = []
+        text = ""
+        for b in surround:
+            info = parser.get_block(b, downloaded_project)
+            blockname = parser.get_block_name(info["opcode"])
+            inputs = info["inputs"]
+            text += blockname
+            if inputs:
+                for each in inputs:
+                    if each == "MESSAGE":
+                        text += " [" + inputs[each][1][1] +"]"
+                    elif each == "SECS" or each == "DURATION" or each == "FROM" or each == "TO":
+                        text += " (" + inputs[each][1][1] + ")"
+
+            text += "\n"
+
+            block_list.append((blockname, info))
+
+        if project_id != "":
+            return render_template("results.html", username=project_id, data=downloaded_project, results=results, block_names=block_names, child=child, sprite=sprite, surround=block_list, text=text)
+
+            
+        return render_template("results.html")
+
 if __name__ == "__main__":
     app.run()

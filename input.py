687349b308e3cb8158961def3ed4a4e2eafa2cda
Amy Zhou <amy_zhou@college.harvard.edu>
2020-04-30 13:46:02 +1000

    update files

--- input.py
+++ input.py
@@ -22,18 +22,32 @@ def project_form_post():
     results = blockify(downloaded_project)
     block_names = convert_block_names(downloaded_project)
 
-    child = parser.get_child_blocks(results["blocks"]["event_whenflagclicked"][0], downloaded_project)
+    block_of_interest = "operator_random"
 
-    sprite = parser.get_sprite(results["blocks"]["event_whenflagclicked"][0], downloaded_project)
+    child = parser.get_child_blocks(results["blocks"][block_of_interest][0], downloaded_project)
+    sprite = parser.get_sprite(results["blocks"][block_of_interest][0], downloaded_project)
+    surround = parser.get_surrounding_blocks(results["blocks"][block_of_interest][0], downloaded_project, 7)
 
-    surround = parser.get_surrounding_blocks(results["blocks"]["event_whenflagclicked"][0], downloaded_project)
     block_list = []
+    text = ""
     for b in surround:
         info = parser.get_block(b, downloaded_project)
-        block_list.append(parser.get_block_name(info["opcode"]))
+        blockname = parser.get_block_name(info["opcode"])
+        inputs = info["inputs"]
+        text += blockname
+        if inputs:
+            for each in inputs:
+                if each == "MESSAGE":
+                    text += " [" + inputs[each][1][1] +"]"
+                elif each == "SECS" or each == "DURATION" or each == "FROM" or each == "TO":
+                    text += " (" + inputs[each][1][1] + ")"
+
+        text += "\n"
+
+        block_list.append((blockname, info))
 
     if project_id != "":
-        return render_template("results.html", username=project_id, data=downloaded_project, results=results, block_names=block_names, child=child, sprite=sprite, surround=block_list)
+        return render_template("results.html", username=project_id, data=downloaded_project, results=results, block_names=block_names, child=child, sprite=sprite, surround=block_list, text=text)
 
 
 

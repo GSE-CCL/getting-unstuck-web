4eca7811aeb71e9d937b426fad7d7ad4aa3059fc
Joshua Archibald <jarchibald121@gmail.com>
2020-06-04 00:06:24 -0500

    Form to find one's project within a studio, and download it if needed.

--- lib/scrape.py
+++ lib/scrape.py
@@ -115,7 +115,7 @@ def add_project(project_id, studio_id=0, cache_directory=None, credentials_file=
         credentials_file (str): path to the database credentials file.
     
     Returns:
-        True, if a new insertion. False, if updated a record. False if Scratch 2.
+        True, if a new insertion or if updated a record. False if Scratch 2.
 
     Raises:
         IOError: if couldn't write the JSON file to the given cache_directory.
@@ -190,7 +190,7 @@ def add_project(project_id, studio_id=0, cache_directory=None, credentials_file=
     doc.save()
     add_comments(project_id, metadata["author"]["username"], credentials_file=credentials_file)
 
-    return not preexisting
+    return True
 
 def add_studio(studio_id, schema=None, show=False, cache_directory=None, credentials_file="secure/db.json"):
     """Scrapes a studio and inserts it into the database.

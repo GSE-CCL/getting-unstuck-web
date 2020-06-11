537f3a9a02e6bcff5153e39fb4086ff5ae73e452
Amy Zhou <amy_zhou@college.harvard.edu>
2020-06-10 21:51:06 -0400

    added homepage and prompt blurb, along with other formatting

--- app.py
+++ app.py
@@ -157,7 +157,11 @@ def edit_schema(id):
 # Studios, projects, users, challenges
 @app.route("/")
 def homepage():
-    return render_template("index.html") 
+    return render_template("home.html") 
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 @app.route("/project/d", methods=["POST"])
 def project_download():
@@ -296,48 +300,6 @@ def prompts():
     return render_template("prompts.html",
                            challenges=studios,
                            schemas=schemas)
-
-@app.route("/challenges-amy", methods=["GET", "POST"])
-def get_challenge():
-    if request.method == "GET":
-        return render_template("submit_challenge.html")
-    else:
-        scraper = Scraper()
-        parser = Parser()
-        project_url = request.form['project-url']
-        project_id = scraper.get_id(project_url)
-        downloaded_project = scraper.download_project(project_id)
-        results = parser.blockify(scratch_data=downloaded_project)
-
-        block_of_interest = "operator_random"
-
-        child = parser.get_child_blocks(results["blocks"][block_of_interest][0], downloaded_project)
-        sprite = parser.get_sprite(results["blocks"][block_of_interest][0], downloaded_project)
-        surround = parser.get_surrounding_blocks(results["blocks"][block_of_interest][0], downloaded_project, 7)
-
-        block_list = []
-        text = ""
-        for b in surround:
-            info = parser.get_block(b, downloaded_project)
-            blockname = parser.get_block_name(info["opcode"])
-            inputs = info["inputs"]
-            text += blockname
-            if inputs:
-                for each in inputs:
-                    if each == "MESSAGE":
-                        text += " [" + inputs[each][1][1] +"]"
-                    elif each == "SECS" or each == "DURATION" or each == "FROM" or each == "TO":
-                        text += " (" + inputs[each][1][1] + ")"
-
-            text += "\n"
-
-            block_list.append((blockname, info))
-
-        if project_id != "":
-            return render_template("results.html", username=project_id, data=downloaded_project, results=results, child=child, sprite=sprite, surround=block_list, text=text)
-
-            
-        return render_template("results.html")
 
 @app.route("/summary", methods=["GET"])
 def summarize():

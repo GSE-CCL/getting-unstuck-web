74cbac8c52b0a38e18e5f561effc8f6b5e823bb9
Joshua Archibald <jarchibald121@gmail.com>
2020-07-06 00:54:39 -0400

    Case insensitive usernames, max height for code excerpts.

--- app.py
+++ app.py
@@ -339,7 +339,7 @@ def studio_id(sid):
 @app.route("/user/<username>")
 def user_id(username):
     common.connect_db()
-    projects = list(scrape.Project.objects(author = username))
+    projects = list(scrape.Project.objects(author = username.lower()))
     studios = dict()
 
     keep_projects = list()

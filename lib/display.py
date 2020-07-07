7e5b0244a4833e3f514a022b846a5dc3ccc1df82
Joshua Archibald <43184986+jsarchibald@users.noreply.github.com>
2020-07-06 23:26:39 -0400

    Prodpatch7 (#154)
    * 5 new projects button relocate
    
    * Responsive 5 new projects
    
    * Prompts formatting update
    
    * Fix display.py description bug
    
    * Check if _reflections cookie is set
    
    * Project result stats overrides
    
    * Navbar fix maybe

--- lib/display.py
+++ lib/display.py
@@ -238,7 +238,7 @@ def get_project_page(pid, cache_directory=settings.CACHE_DIRECTORY):
     # One prompt variable to take the logic out of the templating language
     prompt = {
         "title": sc["title"] if "title" in sc and sc["title"] is not None else studio["title"] if "title" in studio else None,
-        "description": sc["description"] if "description" in sc else studio["description"]
+        "description": sc["description"] if "description" in sc else studio["description"] if "description" in studio else None
     }
 
     # Choose stats to show
@@ -264,6 +264,7 @@ def get_studio_stats(sc, studio):
     parser = Parser()
 
     stats = list()
+    overrides = {"block_count": "blocks used", "number_projects": "projects in the studio"}
 
     if "stats" not in sc:
         return stats
@@ -294,6 +295,10 @@ def get_studio_stats(sc, studio):
             if key == "blocks" or key == "block_categories":
                 append = "blocks"
             else:
+                # Override a few
+                if key in overrides:
+                    key = overrides[key]
+
                 s["name"].append(key.replace("_", " "))
 
         # If studio doesn't have the stat requested

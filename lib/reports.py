69a8faee6be2f29c8956ae80a701bb7f845146cc
Joshua Archibald <jarchibald121@gmail.com>
2020-08-05 15:46:10 -0400

    Style

--- lib/reports.py
+++ lib/reports.py
@@ -10,7 +10,6 @@ from werkzeug.exceptions import NotFound
 
 from . import authentication, schema, scrape, settings
 
-
 reporting = Blueprint("reports", __name__, template_folder="templates")
 
 
@@ -19,7 +18,8 @@ reporting = Blueprint("reports", __name__, template_folder="templates")
 def get_projects():
     """Gets projects as a CSV."""
 
-    level = None if "block_detail" in request.args and request.args["block_detail"] == "all" else 2
+    level = None if "block_detail" in request.args and request.args[
+        "block_detail"] == "all" else 2
 
     common.connect_db()
 
@@ -27,12 +27,23 @@ def get_projects():
 
     try:
         if int(request.args["studio_id"]) > 0:
-            projects = json.loads(scrape.Project.objects(studio_id=request.args["studio_id"]).exclude("id", "cache_expires", "image", "reload_page").to_json())
+            projects = json.loads(
+                scrape.Project.objects(studio_id=request.args["studio_id"])
+                .exclude("id",
+                         "cache_expires",
+                         "image",
+                         "reload_page").to_json())
     except:
-        projects = json.loads(scrape.Project.objects().exclude("id", "cache_expires", "image", "reload_page").to_json())
+        projects = json.loads(scrape.Project.objects()
+                              .exclude("id",
+                                       "cache_expires",
+                                       "image",
+                                       "reload_page").to_json())
 
     for project in projects:
-        reflections = project_reflections.filter(project_id=project["project_id"]).order_by("-id")
+        reflections = project_reflections.filter(
+            project_id=project["project_id"]).order_by("-id")
+
         if reflections.count() > 0:
             reflection = reflections.first()
             project["minutes"] = reflection["minutes"]
@@ -40,19 +51,32 @@ def get_projects():
             for i, feeling in enumerate(reflection["feelings"]):
                 project[f"feeling_{i}"] = feeling
 
-
     flattened = pd.json_normalize(projects, max_level=level)
 
     # Order columns
-    cols = ["project_id", "title", "author", "description", "instructions", "studio_id"]
+    cols = [
+        "project_id",
+        "title",
+        "author",
+        "description",
+        "instructions",
+        "studio_id"
+    ]
+
     stats = list()
     for row in flattened:
         if row not in stats and row not in cols:
             stats.append(row)
+
     cols = cols + sorted(stats)
 
-    response = Response(flattened.to_csv(columns=cols, index=False), mimetype="text/csv")
-    response.headers.set("Content-Disposition", "attachment", filename="projects.csv")
+    response = Response(flattened.to_csv(columns=cols,
+                                         index=False),
+                        mimetype="text/csv")
+
+    response.headers.set("Content-Disposition",
+                         "attachment",
+                         filename="projects.csv")
 
     return response
 
@@ -62,28 +86,45 @@ def get_projects():
 def get_studios():
     """Gets studios as a CSV."""
 
-    level = None if "block_detail" in request.args and request.args["block_detail"] == "all" else 2
+    level = None if "block_detail" in request.args and request.args[
+        "block_detail"] == "all" else 2
 
     common.connect_db()
 
     try:
         if int(request.args["id"]) > 0:
-            studios = json.loads(scrape.Studio.objects(studio_id=request.args["id"]).exclude("id").to_json())
+            studios = json.loads(
+                scrape.Studio.objects(
+                    studio_id=request.args["id"]).exclude("id").to_json())
     except:
         studios = json.loads(scrape.Studio.objects().exclude("id").to_json())
-
 
     flattened = pd.json_normalize(studios, max_level=level)
 
     # Order columns
-    cols = ["studio_id", "title", "description", "status", "challenge_id.$oid", "public_show"]
+    cols = [
+        "studio_id",
+        "title",
+        "description",
+        "status",
+        "challenge_id.$oid",
+        "public_show"
+    ]
+
     stats = list()
     for row in flattened:
         if row not in stats and row not in cols:
             stats.append(row)
+
     cols = cols + sorted(stats)
 
-    response = Response(flattened.to_csv(na_rep=0, columns=cols, index=False), mimetype="text/csv")
-    response.headers.set("Content-Disposition", "attachment", filename="studios.csv")
+    response = Response(flattened.to_csv(na_rep=0,
+                                         columns=cols,
+                                         index=False),
+                        mimetype="text/csv")
+
+    response.headers.set("Content-Disposition",
+                         "attachment",
+                         filename="studios.csv")
 
     return response
